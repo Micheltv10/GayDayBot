89ca35af67709d4cb22f02f2b3811fb4b33bb4b4
Michel <michel.nowacki05@gmail.com>
2024-03-20 19:28:43 +0100

    ADB

--- windowcapture.py
+++ windowcapture.py
@@ -1,11 +1,9 @@
 import numpy as np
-import cv2 
-import pyscreenshot as ImageGrab
-
+import cv2
+from ppadb.client import Client as AdbClient
 
 
 class WindowCapture:
-
     # properties
     w = 1920
     h = 1080
@@ -22,9 +20,19 @@ class WindowCapture:
         self.offset_y = 0
         self.w = 1920
         self.h = 1080
+        self.adb = AdbClient(host="127.0.0.1", port=5037)
+        self.devices = self.adb.devices()
 
-    def get_screenshot(self):
-        frame = ImageGrab.grab(bbox=(self.offset_x , self.cropped_y, self.w, self.h))
-        frame = np.array(frame)
+        if len(self.devices) == 0:
+            # create exception
+            print("No device connected")
+
+        self.device = self.devices[0]
+
+    def get_frame(self):
+        frame = self.device.screencap()  # Take a screenshot
+        # Convert the screenshot to a format that OpenCV can use
+        frame = cv2.imdecode(np.frombuffer(frame, np.uint8), -1)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         return frame
